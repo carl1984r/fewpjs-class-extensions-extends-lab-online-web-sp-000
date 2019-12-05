186f8ab4ba803ccc18d16fcbdc316e41405c7d85
Bry Cox <carl.cc@gmail.com>
2019-12-05 09:49:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+class Polygon {
+
+  constructor(sides) {
+    this.sides = sides;
+  };
+
+  get countSides() {
+    return this.sides.length;
+  };
+
+  get perimeter() {
+    return this.sides.reduce((acc, val) => acc + val);
+  };
+
+}
